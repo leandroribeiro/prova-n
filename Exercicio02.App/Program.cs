00793baaa7ecbed1e746e868a1541f5d02fb4b1b
Leandro Ribeiro <github@leandroribeiro.com>
2020-09-07 12:37:01 -0300

    Configurando Polly para retentativas de conexão do Exercicio 2

--- Exercicio02.App/Program.cs
+++ Exercicio02.App/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Data.SqlClient;
 using Dapper;
+using Polly;
 
 namespace Exercicio02.App
 {
@@ -12,47 +13,93 @@ namespace Exercicio02.App
         {
             var connection = "Server=exercicio02_database;Database=Exercicio02;User Id=sa;Password=Dev123456789";
 
-            using (var conexaoBD = new SqlConnection(connection))
+            
+            Policy retryPolicy = Policy.Handle<SqlException>().WaitAndRetry(
+                retryCount: 3,
+                sleepDurationProvider: attempt => TimeSpan.FromMilliseconds(5000));
+            
+            retryPolicy.Execute(() =>
             {
-                Console.WriteLine("Query 1");
-                
-                string query = @"
-                select Nome
-                from Empregado
+                using (var conexaoBD = new SqlConnection(connection))
+                {
+                    ExecuteQuery1(conexaoBD);
+
+                    Console.WriteLine("");
+                    ExecuteQuery2(conexaoBD);
+
+                    Console.WriteLine("");
+                    ExecuteQuery3(conexaoBD);
+
+                    Console.WriteLine("");
+                    ExecuteQuery4(conexaoBD);
+                }
+            });
+
+        }
+
+        private static void ExecuteQuery4(SqlConnection conexaoBD)
+        {
+            string query;
+            Console.WriteLine("Query 4");
+
+            query = @"
+                select 
+                    Nome 
+                from 
+                    Empregado 
+                where 
+                    Codigo in ( select E1.Codigo from Empregado E1, Empregado E2 where E1.Salario > E2.Salario)";
+
+            ExecuteQuery(conexaoBD, query);
+        }
+
+        private static void ExecuteQuery3(SqlConnection conexaoBD)
+        {
+            string query;
+            Console.WriteLine("Query 3");
+
+            query = @"
+                    select 
+                        Nome
+                    from 
+                        Empregado 
+                    where 
+                        Salario >= all (select Salario from Empregado)";
+
+            ExecuteQuery(conexaoBD, query);
+        }
+
+        private static void ExecuteQuery2(SqlConnection conexaoBD)
+        {
+            string query;
+            Console.WriteLine("Query 2");
+
+            query = @"
+                    select
+                        Nome 
+                    from 
+                        Empregado
+                    where Salario = (select max(Salario) from Empregado)";
+
+            ExecuteQuery(conexaoBD, query);
+        }
+
+        private static void ExecuteQuery1(SqlConnection conexaoBD)
+        {
+            Console.WriteLine("Query 1");
+
+            string query = @"
+                select 
+                    Nome
+                from 
+                    Empregado
                 where Codigo in (select Codigo
                     from Empregado)
                     AND Codigo NOT IN ((select E1.Codigo
                     from Empregado E1, Empregado E2
                     where E1.Salario < E2.Salario));";
 
-                ExecuteQuery(conexaoBD, query);
-
-                Console.WriteLine("");
-                Console.WriteLine("Query 2");
-                
-                query = @"select Nome from Empregado
-where Salario = (select max(Salario) from Empregado)";
-
-                ExecuteQuery(conexaoBD, query);
-
-                Console.WriteLine("");
-                Console.WriteLine("Query 3");
-                
-                query = @"select Nome from Empregado
-where Salario >= all (select Salario from Empregado)";
-
-                ExecuteQuery(conexaoBD, query);
-
-                Console.WriteLine("");
-                Console.WriteLine("Query 4");
-                
-                query = @"select Nome from Empregado
-where Codigo in ( select E1.Codigo from Empregado E1, Empregado E2
-where E1.Salario > E2.Salario
-)";
-
-                ExecuteQuery(conexaoBD, query);
-            }
+            ExecuteQuery(conexaoBD, query);
         }
 
         private static void ExecuteQuery(SqlConnection conexaoBD, string query1)
--- Exercicio02/initialize-database.sh
+++ Exercicio02/initialize-database.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
- echo antes && sleep 15 && echo depois && /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d master -i /home/setup.sql
+ sleep 10 && /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d master -i /home/setup.sql
